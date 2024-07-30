59a79bba561031d021312e79443f78518ba1a626
piyushpatil2002 <piyushpatil2002@gmail.com>
2024-07-30 20:43:01 +0530

    typewriter

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,8 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Student",
-          "Developer",
+          "Front-End Developer",
+          "Mobile-App Developer",
         ],
         autoStart: true,
         loop: true,
